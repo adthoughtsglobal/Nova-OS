b49c5a248953b341283bed52ac7a55e0280d7aa4
adthoughtsglobal <123654929+adthoughtsglobal@users.noreply.github.com>
2024-07-01 22:16:54 +0530

    new files app, flex window ui, Rutor for Nova

--- nova.js
+++ nova.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // rotur
 
 let username = "nva";
@@ -203,47 +202,4 @@ function sendHandshake() {
   }
 
   ws.send(JSON.stringify(msg));
-=======
-var novaos = {
-    shrinkString: function(str) {
-        return window.parent.shrinkbsf(str)
-    },
-    unshrinkString: function(str) {
-        return window.parent.unshrinkbsf(str)
-    },
-    ask: function(str) {
-        return window.parent.ask(str)
-    },
-    say: function(str) {
-        return window.parent.say(str)
-    },
-    justConfirm: function(...args) {
-        return window.parent.justConfirm(args)
-    },
-    openFile: function(...args) {
-        return window.parent.openfile(args)
-    },
-    OpenL: function(...args) {
-        return window.parent.openlaunchprotocol(args)
-    },
-    notify: function(...args) {
-        return window.parent.notify(args)
-    },
-    appInstances: function() {
-        return window.parent.winds;
-    },
-    roturConnect: function() {
-        var ws = new WebSocket("ws://localhost:8080");
-        ws.onopen = function() {
-            ws.send("NovaOS connected to Rotur");
-        };
-        ws.onmessage = function (evt) {
-            console.log(evt.data);
-        };
-        ws.onclose = function() {
-            console.log("Connection closed");
-        };
-        
-    }
->>>>>>> 9331ed507497f77c25b5543c5a70709108cc964d
 }
