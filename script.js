9cfcaa12e29d5091933635071c34a3af780f0865
adthoughtsglobal <adthoughtsglobal@gmail.com>
2024-08-02 00:07:06 +0530

    menifest updates, kernal and fixed app assosisations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var batteryLevel, winds = {}, rp, flwint = true, memory, _nowapp, fulsapp = false, nowappdo, appsHistory = [], nowwindow, appicns = {}, dev = true, appfound = 'files', fileslist = [], qsetscache = {};
-var really = false, initmenuload = true, fileTypeAssociations = {};;
+var really = false, initmenuload = true, fileTypeAssociations = {}, Gtodo;
 var novaFeaturedImage = `https://images.unsplash.com/photo-1716980197262-ce400709bf0d?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`;
 
 document.getElementById("bgimage").src = novaFeaturedImage;
@@ -528,7 +528,7 @@ async function dod() {
 			// Create a div element for the app shortcut
 			var appShortcutDiv = document.createElement("div");
 			appShortcutDiv.className = "app-shortcut sizableuielement";
-			appShortcutDiv.setAttribute("onclick", "openfile(this)");
+			appShortcutDiv.setAttribute("onclick", "openfile('"+app.id+"')");
 			appShortcutDiv.setAttribute("unid", app.id);
 
 
@@ -716,38 +716,6 @@ function getAppTheme(unshrunkContent) {
 	return metaTagData;
 }
 
-async function openapp(x, od) {
-
-	if (gid('appdmod').open) {
-		gid('appdmod').close()
-	}
-	if (gid('searchwindow').open) {
-		gid('searchwindow').close()
-	}
-
-	const fetchDataAndSave = async (x) => {
-		try {
-			var y;
-			if (od != 1) {
-				y = await getFileById(od)
-				y = unshrinkbsf(y.content)
-			} else {
-				y = await fetchData("appdata/" + x + ".html");
-				let m = await getFileNamesByFolder("Apps");
-				await createFile("Apps", x, "app", y);
-			}
-
-			// Assuming you have a predefined function openwindow
-			openwindow(x, y, getAppIcon(y, x), getAppTheme(y));
-		} catch (error) {
-			console.error("Error fetching data:", error);
-		}
-	};
-
-	// Call fetchDataAndSave with the desired value of x
-	fetchDataAndSave(x);
-}
-
 async function fetchData(url) {
 	try {
 		const response = await fetch(url);
@@ -763,216 +731,6 @@ async function fetchData(url) {
 	}
 }
 var content;
-function openwindow(title, cont, ic, theme, appid) {
-	appsHistory.push(title)
-	if (appsHistory.length > 5) {
-		appsHistory = appsHistory.slice(-5);
-	}
-
-	if (winds.length > 3) {
-		notify("Over 100 tabs is open.", "Even if your pc can handle it - this is cringe.")
-	}
-
-	content = cont
-	if (content == undefined) {
-		content = "<center><h1>Unavailable</h1>App Data cannot be read.</center>";
-	}
-
-	let winuid = genUID();
-	winds[title + winuid] = 1;
-
-	// Create the window element
-	var windowDiv = document.createElement("div");
-	windowDiv.id = "window" + winuid;
-
-
-	windowDiv.setAttribute("data-winds", title + winuid);
-	windowDiv.onclick = function () {
-		nowapp = title;
-		dewallblur();
-	}
-	nowapp = title;
-	dewallblur();
-	windowDiv.classList += "window";
-
-	let isitmob = window.innerWidth <= 500;
-
-	if (!isitmob) {
-		windowDiv.style = 'left: calc(50vw - 33.5vw); top: calc(50vh - 35vh); width: 65vw; height: 70vh; z-index: 0;';
-
-		// Get the computed style of the windowDiv
-		let computedStyle = getComputedStyle(windowDiv);
-
-		// Get the current positions calculated by the browser
-		let currentLeft = parseFloat(computedStyle.left);
-		let currentTop = parseFloat(computedStyle.top);
-
-		// Update the positions
-		let newLeft = `calc(50vw - 33.5vw + ${5 * Object.keys(winds).length}px)`;
-		let newTop = `calc(50vh - 35vh + ${5 * Object.keys(winds).length}px)`;
-
-		windowDiv.style.left = newLeft;
-		windowDiv.style.top = newTop;
-
-	} else {
-		windowDiv.style.left = 0;
-		windowDiv.style.top = 0;
-		windowDiv.style.width = 'calc(100% - 0px)';
-		windowDiv.style.height = 'calc(100% - 58px)';
-	}
-
-	// Create the window header
-	var windowHeader = document.createElement("div");
-	windowHeader.id = "window" + winuid + "header"
-	windowHeader.classList += "windowheader";
-	let windowdataspan = document.createElement("div");
-	windowdataspan.classList += "windowdataspan";
-	windowdataspan.innerHTML = ic != null ? ic : "";
-	let windowtitlespan = document.createElement("div");
-	windowtitlespan.innerHTML += toTitleCase(basename(title));
-	windowdataspan.appendChild(windowtitlespan);
-	windowHeader.appendChild(windowdataspan);
-	if (theme != null) {
-		windowHeader.style.backgroundColor = theme;
-		windowDiv.style.border = `1px solid ` + theme;
-		if (isDark(theme)) {
-			windowHeader.style.color = "white";
-		} else {
-			windowHeader.style.color = "black";
-		}
-	}
-	windowHeader.setAttribute("title", title + winuid)
-	windowHeader.addEventListener("mouseup", function (event) {
-		checksnapping(windowDiv, event);
-	});
-
-	windowHeader.addEventListener("mousedown", function (event) {
-		putwinontop('window' + winuid);
-		winds[title + winuid] = windowDiv.style.zIndex;
-
-	});
-
-	var ibtnsside = document.createElement("div");
-	ibtnsside.classList += "ibtnsside"
-
-	var flButton = document.createElement("span");
-	flButton.classList.add("material-symbols-rounded", "wincl", "flbtn");
-	flButton.style = `    padding: 4px 5px;
-    font-size: 8px !important;`;
-	flButton.textContent = "open_in_full";
-	flButton.onclick = function () {
-		flwin(flButton);
-	};
-
-	// Create the close button in the header
-	var closeButton = document.createElement("span");
-	closeButton.classList.add("material-symbols-rounded", "wincl");
-	closeButton.textContent = "close";
-	closeButton.onclick = function () {
-		setTimeout(function () {
-			dewallblur();
-		}, 500);
-		clwin("window" + winuid);
-		delete winds[title + winuid];
-		loadtaskspanel()
-	};
-
-	// Append the close button to the header
-	ibtnsside.appendChild(closeButton);
-	if (!isitmob) { ibtnsside.appendChild(flButton); }
-
-	windowHeader.appendChild(ibtnsside);
-
-	var windowContent = document.createElement("div");
-	windowContent.classList += "windowcontent";
-
-	var windowLoader = document.createElement("div");
-	windowLoader.classList += "windowloader";
-	var loaderdiv = document.createElement("div");
-	loaderdiv.classList = "loader33";
-	windowLoader.innerHTML = appicns[title] ? appicns[title] : defaultAppIcon;
-	windowLoader.appendChild(loaderdiv);
-
-
-	function loadIframeContent(windowLoader, windowContent, iframe) {
-		var iframe = document.createElement("iframe");
-		var contentString = content.toString();
-
-		// Function to check if a string is Base64 encoded
-		function isBase64(str) {
-			try {
-				return btoa(atob(str)) === str;
-			} catch (err) {
-				return false;
-			}
-		}
-
-		// Decode the content string if it's Base64 encoded
-		if (isBase64(contentString)) {
-			contentString = atob(contentString);
-		}
-
-		// Create a Blob from the content string
-		var blob = new Blob([contentString], { type: 'text/html' });
-
-		// Create a URL for the Blob
-		var blobURL = URL.createObjectURL(blob);
-
-		iframe.onload = function () {
-			var doc = iframe.contentDocument || iframe.contentWindow.document;
-
-			iframe.contentWindow.myWindow = {
-				"element": windowDiv,
-				"titleElement": windowtitlespan,
-				"appID":appid
-			};
-
-			// Check if the content string contains the function greenflag
-			if (contentString.includes(`function greenflag()`)) {
-				attemptGreenFlag(windowLoader, windowContent, iframe);
-			} else {
-				windowLoader.remove();
-			}
-		};
-
-		// Set the src of the iframe to the Blob URL
-		iframe.src = blobURL;
-
-		windowContent.appendChild(iframe);
-	}
-
-	function attemptGreenFlag(windowLoader, windowContent, iframe) {
-		try {
-			iframe.contentWindow.greenflag();
-			windowLoader.style.display = "none";
-			windowLoader.remove();
-		} catch (error) {
-			console.log(error)
-			if (!String(error.message).includes('greenflag')) {
-				windowLoader.style.display = "none";
-				return;
-			}
-
-
-		}
-	}
-
-	nowwindow = 'window' + winuid;
-	// Append the header and content to the window
-	windowDiv.appendChild(windowHeader);
-	windowDiv.appendChild(windowContent);
-	windowDiv.appendChild(windowLoader);
-
-	// Append the window to the document body
-	document.body.appendChild(windowDiv);
-
-	// Initial load
-	loadIframeContent(windowLoader, windowContent);
-
-	dragElement(windowDiv);
-	putwinontop('window' + winuid);
-	loadtaskspanel();
-}
 
 function putwinontop(x) {
 	if (Object.keys(winds).length > 1) {
@@ -994,17 +752,6 @@ function toTitleCase(str) {
 	return str.toLowerCase().replace(/(?:^|\s)\w/g, function (match) {
 		return match.toUpperCase();
 	});
-}
-
-async function openlaunchprotocol(x, y) {
-	x = {
-		"appname": x,
-		"data": y
-	};
-	localStorage.setItem("todo", JSON.stringify(x));
-	let m;
-	m = await getFileByPath("Apps/" + x.appname);
-	openfile(m);
 }
 
 function requestLocalFile() {
@@ -1150,19 +897,16 @@ async function createFile(folderName2, fileName, type, content, metadata = {}) {
 
 async function extractAndRegisterCapabilities(appId, content) {
     try {
-        // Decode Base64 content if necessary
         if (isBase64(content)) {
             content = atob(content);
         }
 
-        // Parse the HTML content to extract the meta tag
         let parser = new DOMParser();
         let doc = parser.parseFromString(content, "text/html");
         let metaTag = doc.querySelector('meta[name="capabilities"]');
 
         if (metaTag) {
             let capabilities = metaTag.getAttribute("content").split(',');
-            // Register the app's capabilities using appId
             await registerApp(appId, capabilities);
             console.log(`Registered capabilities for app ID: ${appId}`);
         } else {
@@ -1174,18 +918,19 @@ async function extractAndRegisterCapabilities(appId, content) {
 }
 
 async function registerApp(appId, capabilities) {
-    fileTypeAssociations[appId] = capabilities;
+    for (let fileType of capabilities) {
+        fileTypeAssociations[fileType] = appId;
+    }
     await setSetting('fileTypeAssociations', fileTypeAssociations);
 }
 
 async function cleanupInvalidAssociations() {
     const validAppIds = await getAllValidAppIds();
 
-    // Iterate over the fileTypeAssociations object and clean up invalid associations
-    for (let appId in fileTypeAssociations) {
-        // If the appId is not valid, remove it from the associations
+    for (let fileType in fileTypeAssociations) {
+        let appId = fileTypeAssociations[fileType];
         if (!validAppIds.includes(appId)) {
-            delete fileTypeAssociations[appId];
+            delete fileTypeAssociations[fileType];
         }
     }
 
@@ -1940,55 +1685,6 @@ document.addEventListener('click', (event) => {
 		dash.close();
 	}
 });
-
-
-async function openfile(x) {
-	console.log("opening: " + x)
-	let unid= x;
-	try {
-		if (!unid) {
-			console.error("Error: 'unid' not found");
-			return;
-		}
-
-		let mm = await getFileById(unid);
-		if (!mm) {
-			console.error("Error: File not found");
-			return;
-		}
-		mm.type = ptypext(mm.fileName);
-
-		if (mm.type == "app") {
-			await openapp(mm.fileName, unid);
-		} else {
-			if (mm.type == "osl") {
-				runAsOSL(mm.content)
-			} else if (mm.type == "lnk") {
-				let z = JSON.parse(mm.content);
-				openfile(z.open)
-			} else {
-				let appIdToOpen = null;
-				const fileExtension = mm.fileName.substring(mm.fileName.lastIndexOf('.'));
-
-				for (let appId in fileTypeAssociations) {
-					if (fileTypeAssociations[appId].includes(fileExtension)) {
-						appIdToOpen = appId;
-						break;
-					}
-				}
-			
-				if (!appIdToOpen) {
-					say('No apps in your system can open this file type.', "failed")
-				} else {
-					openlaunchprotocol(appIdToOpen, JSON.stringify({"lclfile": unid}));
-				}
-			}
-		}
-	} catch (error) {
-		console.error(":( Error:", error);
-		say("<h1>Unable to open file</h1>File Error: " + error, "failed")
-	}
-}
 
 async function dewallblur() {
 	if (!await getSetting("focusMode")) {
---
+++ scripts/kernel.js
@@ -0,0 +1,299 @@
+async function openlaunchprotocol(appid, data) {
+	console.log("Open Lanuch Protocol", appid, data)
+	let x = {
+		"appid": appid,
+		"data": data
+	};
+	Gtodo = x;
+	openfile(x.appid, {data: Gtodo});
+}
+
+
+async function openfile(x) {
+    var dataavailable = {};
+	console.log("opening: " + x)
+	let unid= x;
+	try {
+		if (!unid) {
+			console.error("No app id provided");
+			return;
+		}
+
+		let mm = await getFileById(unid);
+		// mm is the file
+		if (!mm) {
+			console.error("Error: File not found");
+			return;
+		}
+		// extract type from file extension
+		mm.type = ptypext(mm.fileName);
+
+		if (mm.type == "app") {
+			// run the app if it is one
+			await openapp(mm.fileName, unid);
+		} else if (mm.type == "osl") {
+			runAsOSL(mm.content)
+		} else if (mm.type == "lnk") {
+			let z = JSON.parse(mm.content);
+			openfile(z.open)
+		} else {
+            // Not a .lnk file or an .osl file nor an .app file.
+            let appIdToOpen = null;
+            const fileExtension = mm.fileName.substring(mm.fileName.lastIndexOf('.'));
+
+            appIdToOpen = fileTypeAssociations[fileExtension] || null;
+		
+			if (!appIdToOpen) {
+				say('No apps in your system can open this file type.', "failed")
+			} else {
+				openlaunchprotocol(appIdToOpen, unid);
+			}
+		}
+	} catch (error) {
+		console.error(":( Error:", error);
+		say("<h1>Unable to open file</h1>File Error: " + error, "failed")
+	}
+}
+
+function openwindow(title, cont, ic, theme, appid, params) {
+	appsHistory.push(title)
+	if (appsHistory.length > 5) {
+		appsHistory = appsHistory.slice(-5);
+	}
+
+	if (winds.length > 3) {
+		notify("Over 100 tabs is open.", "Even if your pc can handle it - this is cringe.")
+	}
+
+	content = cont
+	if (content == undefined) {
+		content = "<center><h1>Unavailable</h1>App Data cannot be read.</center>";
+	}
+
+	let winuid = genUID();
+	winds[title + winuid] = 1;
+
+	// Create the window element
+	var windowDiv = document.createElement("div");
+	windowDiv.id = "window" + winuid;
+
+
+	windowDiv.setAttribute("data-winds", title + winuid);
+	windowDiv.onclick = function () {
+		nowapp = title;
+		dewallblur();
+	}
+	nowapp = title;
+	dewallblur();
+	windowDiv.classList += "window";
+
+	let isitmob = window.innerWidth <= 500;
+
+	if (!isitmob) {
+		windowDiv.style = 'left: calc(50vw - 33.5vw); top: calc(50vh - 35vh); width: 65vw; height: 70vh; z-index: 0;';
+
+		// Get the computed style of the windowDiv
+		let computedStyle = getComputedStyle(windowDiv);
+
+		// Get the current positions calculated by the browser
+		let currentLeft = parseFloat(computedStyle.left);
+		let currentTop = parseFloat(computedStyle.top);
+
+		// Update the positions
+		let newLeft = `calc(50vw - 33.5vw + ${5 * Object.keys(winds).length}px)`;
+		let newTop = `calc(50vh - 35vh + ${5 * Object.keys(winds).length}px)`;
+
+		windowDiv.style.left = newLeft;
+		windowDiv.style.top = newTop;
+
+	} else {
+		windowDiv.style.left = 0;
+		windowDiv.style.top = 0;
+		windowDiv.style.width = 'calc(100% - 0px)';
+		windowDiv.style.height = 'calc(100% - 58px)';
+	}
+
+	// Create the window header
+	var windowHeader = document.createElement("div");
+	windowHeader.id = "window" + winuid + "header"
+	windowHeader.classList += "windowheader";
+	let windowdataspan = document.createElement("div");
+	windowdataspan.classList += "windowdataspan";
+	windowdataspan.innerHTML = ic != null ? ic : "";
+	let windowtitlespan = document.createElement("div");
+	windowtitlespan.innerHTML += toTitleCase(basename(title));
+	windowdataspan.appendChild(windowtitlespan);
+	windowHeader.appendChild(windowdataspan);
+	if (theme != null) {
+		windowHeader.style.backgroundColor = theme;
+		windowDiv.style.border = `1px solid ` + theme;
+		if (isDark(theme)) {
+			windowHeader.style.color = "white";
+		} else {
+			windowHeader.style.color = "black";
+		}
+	}
+	windowHeader.setAttribute("title", title + winuid)
+	windowHeader.addEventListener("mouseup", function (event) {
+		checksnapping(windowDiv, event);
+	});
+
+	windowHeader.addEventListener("mousedown", function (event) {
+		putwinontop('window' + winuid);
+		winds[title + winuid] = windowDiv.style.zIndex;
+
+	});
+
+	var ibtnsside = document.createElement("div");
+	ibtnsside.classList += "ibtnsside"
+
+	var flButton = document.createElement("span");
+	flButton.classList.add("material-symbols-rounded", "wincl", "flbtn");
+	flButton.style = `    padding: 4px 5px;
+    font-size: 8px !important;`;
+	flButton.textContent = "open_in_full";
+	flButton.onclick = function () {
+		flwin(flButton);
+	};
+
+	// Create the close button in the header
+	var closeButton = document.createElement("span");
+	closeButton.classList.add("material-symbols-rounded", "wincl");
+	closeButton.textContent = "close";
+	closeButton.onclick = function () {
+		setTimeout(function () {
+			dewallblur();
+		}, 500);
+		clwin("window" + winuid);
+		delete winds[title + winuid];
+		loadtaskspanel()
+	};
+
+	// Append the close button to the header
+	ibtnsside.appendChild(closeButton);
+	if (!isitmob) { ibtnsside.appendChild(flButton); }
+
+	windowHeader.appendChild(ibtnsside);
+
+	var windowContent = document.createElement("div");
+	windowContent.classList += "windowcontent";
+
+	var windowLoader = document.createElement("div");
+	windowLoader.classList += "windowloader";
+	var loaderdiv = document.createElement("div");
+	loaderdiv.classList = "loader33";
+	windowLoader.innerHTML = appicns[title] ? appicns[title] : defaultAppIcon;
+	windowLoader.appendChild(loaderdiv);
+
+
+	function loadIframeContent(windowLoader, windowContent, iframe) {
+		var iframe = document.createElement("iframe");
+		var contentString = content.toString();
+
+		// Function to check if a string is Base64 encoded
+		function isBase64(str) {
+			try {
+				return btoa(atob(str)) === str;
+			} catch (err) {
+				return false;
+			}
+		}
+
+		// Decode the content string if it's Base64 encoded
+		if (isBase64(contentString)) {
+			contentString = atob(contentString);
+		}
+
+		// Create a Blob from the content string
+		var blob = new Blob([contentString], { type: 'text/html' });
+
+		// Create a URL for the Blob
+		var blobURL = URL.createObjectURL(blob);
+
+		iframe.onload = function () {
+			var doc = iframe.contentDocument || iframe.contentWindow.document;
+
+			iframe.contentWindow.myWindow = {
+				element: windowDiv,
+				titleElement: windowtitlespan,
+				appID: appid,
+				...(params && { params })
+			};
+
+			// Check if the content string contains the function greenflag
+			if (contentString.includes(`function greenflag()`)) {
+				attemptGreenFlag(windowLoader, windowContent, iframe);
+			} else {
+				windowLoader.remove();
+			}
+		};
+
+		// Set the src of the iframe to the Blob URL
+		iframe.src = blobURL;
+
+		windowContent.appendChild(iframe);
+	}
+
+	function attemptGreenFlag(windowLoader, windowContent, iframe) {
+		try {
+			iframe.contentWindow.greenflag();
+			windowLoader.style.display = "none";
+			windowLoader.remove();
+		} catch (error) {
+			console.log(error)
+			if (!String(error.message).includes('greenflag')) {
+				windowLoader.style.display = "none";
+				return;
+			}
+
+
+		}
+	}
+
+	nowwindow = 'window' + winuid;
+	// Append the header and content to the window
+	windowDiv.appendChild(windowHeader);
+	windowDiv.appendChild(windowContent);
+	windowDiv.appendChild(windowLoader);
+
+	// Append the window to the document body
+	document.body.appendChild(windowDiv);
+
+	// Initial load
+	loadIframeContent(windowLoader, windowContent);
+
+	dragElement(windowDiv);
+	putwinontop('window' + winuid);
+	loadtaskspanel();
+}
+
+async function openapp(x, od) {
+    // od is the app id, x is the app name
+        if (gid('appdmod').open) {
+            gid('appdmod').close()
+        }
+        if (gid('searchwindow').open) {
+            gid('searchwindow').close()
+        }
+    
+        const fetchDataAndSave = async (x) => {
+            try {
+                var y;
+                if (od != 1) {
+                    y = await getFileById(od)
+                    y = unshrinkbsf(y.content)
+                } else {
+                    y = await fetchData("appdata/" + x + ".html");
+                    await createFile("Apps", x, "app", y);
+                }
+    
+                // Assuming you have a predefined function openwindow
+                openwindow(x, y, getAppIcon(y, x), getAppTheme(y), od, Gtodo);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+    
+        // Call fetchDataAndSave with the desired value of x
+        fetchDataAndSave(x);
+    }
